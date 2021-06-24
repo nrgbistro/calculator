d3826c8ac2e33463878a240d2deefaaa5927b343
nrgbistro <nolangelinas@gmail.com>
2021-06-24 03:42:48 -0400

    Add updateOutput, style some divs, start creating equalsButton

--- scrips.js
+++ scrips.js
@@ -1,72 +1,104 @@
+// Set constants by IDs
+const numberContainer = document.querySelector("#numberContainer");
+const operatorContainer = document.querySelector("#operatorContainer");
+const symbols = ["+", "-", "*", "/", "="];
+const idNames = ["plus", "minus", "multiply", "divide", "equals"];
+
+
+// Memory for current operation
 let mem = "";
 
-
 function add(n1, n2) {
-    return n1 + n2;
+	return n1 + n2;
 }
 
 function subtract(n1, n2) {
-    return n1 - n2;
+	return n1 - n2;
 }
 
 function multiply(n1, n2) {
-    return n1 * n2;
+	return n1 * n2;
 }
 
 function divide(n1, n2) {
-    return n1 / n2;
+	return n1 / n2;
 }
 
 function operate(operator, n1, n2) {
-    switch (operator) {
-        case "+":
-            return add(n1, n2);
-        case "-":
-            return subtract(n1, n2);
-        case "*":
-            return multiply(n1, n2);
-        case "/":
-            return ret = divide(n1, n2);
-        default:
-            console.log("NO OPERATOR");
-            return;
-    }
+	switch (operator) {
+		case "+":
+			return add(n1, n2);
+		case "-":
+			return subtract(n1, n2);
+		case "*":
+			return multiply(n1, n2);
+		case "/":
+			return divide(n1, n2);
+		default:
+			console.log("NO OPERATOR");
+			return;
+	}
 }
 
 function init() {
-    // Set constants by IDs
-    const numberContainer = document.querySelector("#numberContainer");
-    const operatorContainer = document.querySelector("#operatorContainer");
 
-    // Numbers button setup
-    for (let i = 1; i <= 9; i++) {
-        // Create new button with id of i
-        let newButton = document.createElement("div");
-        newButton.id = `${i}`;
-        numberContainer.appendChild(newButton);
 
-        // Jquery selector for new button
-        let $button = $(`#${i}`);
-        $button.text(`${i}`);
+	// Numbers button setup
+	for (let i = 1; i <= 9; i++) {
+		// Create new button with id of i
+		createButton(i, "#numberContainer");
+	}
 
-        // Click event for number buttons
-        $button.click(function(n) {
-            mem += n.currentTarget.id;
-        });
-    }
-
-    // Operator button setup
-    for (let i = 0; i < 5; i++) {
-        const symbolsArray = ["+", "-", "X", "/", "="];
-        let operatorButton = document.createElement("div");
-        operatorButton.classList.add("operator");
-        operatorButton.id = `${symbolsArray[i]}`;
-        operatorButton.textContent = `${symbolsArray[i]}`;
-        operatorContainer.appendChild(operatorButton);
-    }
+	// Operator button setup
+	for (let i = 0; i < 5; i++) {
+		// Create new button with id of currentSymbol
+		const currentSymbol = "#" + symbols[i];
+		createButton(i, "#operatorContainer");
+	}
 }
+
+
+// Creates most buttons on the calculator
+function createButton(i, target) {
+	let newButton = document.createElement("div");
+	let $button = null;
+	if (target === "#operatorContainer" && symbols[i] === "=") {
+		createEqualsButton();
+	} else if (target === "#numberContainer") {
+		newButton.id = String(i);
+		document.querySelector(target).appendChild(newButton);
+
+		// Jquery selector for new button
+		$button = $("#" + i);
+		$button.text(i);
+	} else {
+		newButton.id = idNames[i];
+		document.querySelector(target).appendChild(newButton);
+
+		// Jquery selector for new button
+		$button = $("#" + idNames[i]);
+		$button.text(symbols[i]);
+	}
+
+	// Click event for operator buttons
+	$button.click(function (n) {
+		mem += n.currentTarget.textContent;
+		updateOutput();
+	});
+}
+
+// Creates equals button on calculator
+function createEqualsButton() {
+
+}
+
 
 $(document).ready();
 {
-    init();
+	init();
+}
+
+
+function updateOutput() {
+	$("#output").children(0).text(mem);
 }
